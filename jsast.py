a39ba3a7a36e981ab9e952ff37327831a87c3bb1
Andrew McCreight <continuation@gmail.com>
2024-03-08 08:06:37 -0800

    remove comment I accidentally copied from elsewhere

--- jsast.py
+++ jsast.py
@@ -4,10 +4,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# The world's worst JS parser. The goal is to parse JSON-y JS produced
-# by JS_ValueToSource. Some of the Ply tricks in here are taken from
-# Firefox's IPDL parser.
-
 class JSNull:
     def __str__(self):
         return "null"
