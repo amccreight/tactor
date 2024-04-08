c30bb03cfe6f7a1a7c7177815bdf0c74427753fe
Andrew McCreight <continuation@gmail.com>
2024-04-08 09:32:24 -0700

    implement basic fallback census

--- logparse_infer.py
+++ logparse_infer.py
@@ -9,16 +9,14 @@
 # This is intended to work with the C++-based type logging from the
 # prototype version of bug 1885221 as of April 8, 2024.
 #
-# The two main goals:
+# Goals:
 # 1) Extract the types for individual messages and combine them
 #   together into types we can use as specifications for the messages/
 # 2) Figure out which cases for fallback serialization (represented
 #   by the "Any" type should be implemented. These will show up as
 #   "AnyToJSIPCValue fallback with" warnings.
+# 3) Tracking "Failed to serialize" warnings might be useful, too.
 
-NS_WARNING("AnyToJSIPCValue fallback with ESClass::Number");
-
-NS_WARNING("Failed to serialize");
 
 import argparse
 import re
@@ -27,14 +25,37 @@ import json
 from jsparse import parseJS, ParseError
 from type import jsValToType
 
-messagePatt = re.compile('QQQ ACTOR ([^ ]+) MESSAGE ([^ ]+) CONTENTS (.+)$')
+
+typePatt = re.compile('JSIT (Send|Recv) ACTOR ([^ ]+) MESSAGE ([^ ]+) TYPE (.+)$')
+# This can also be CONTENTS instead of TYPE, and then it will have the result
+# of toSource(), but I'm not logging that right now so don't worry about it.
+
+# Ideally, we'd report the file and test these warnings happened during.
+fallbackWarningPatt = re.compile('WARNING: AnyToJSIPCValue fallback with (.+): file')
+failedToSerializeWarningPatt = re.compile('WARNING: Failed to serialize')
+
 
 def lookAtActors(args):
     sys.stdin.reconfigure(encoding='latin1')
 
     actors = {}
 
+    fallbackWith = {}
+    failedSerialize = 0
+
     for l in sys.stdin:
+        fwp = fallbackWarningPatt.search(l)
+        if fwp:
+            failCase = fwp.group(1)
+            if failCase in fallbackWith:
+                fallbackWith[failCase] += 1
+            else:
+                fallbackWith[failCase] = 1
+            continue
+        ftswp = failedToSerializeWarningPatt.search(l)
+        if ftswp:
+            failedSerialize += 1
+        continue
         mp = messagePatt.search(l)
         if not mp:
             continue
@@ -81,6 +102,15 @@ def lookAtActors(args):
                 currTypes[foundAt] = t
             else:
                 currTypes.append(t)
+
+    for t, c in fallbackWith.items():
+        print(f"Fallback with {t}; count: {c}")
+    # I disabled the warning for ESClass::Other because it was spammy in one
+    # log I looked at, so this is so I don't forget.
+    print(f"Fallback with ESClass::Other; count: ???")
+    print()
+    print(f"Failed to serialize count: {failedSerialize}")
+    return
 
     for a, mm in actors.items():
         print(a)
